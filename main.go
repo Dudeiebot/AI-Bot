d95d35eb7b041da042204ceeb202239cb7e30892
Dudeiebot <ojoexcel5@gmail.com>
2023-06-12 15:54:45 +0100

    app

--- main.go
+++ main.go
@@ -1,12 +1,20 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/joho/godotenv"
+	"github.com/krognol/go-wolfram"
 	"github.com/shomali11/slacker"
+	"github.com/tidwall/gjson"
+	witai "github.com/wit-ai/wit-go/v2"
 )
+
+var wolframClient *wolfram.Client
 
 func printCommandEvents(analyticsChannel <-chan *slacker.CommandEvent) {
 	for event := range analyticsChannel {
@@ -23,6 +31,38 @@ func main() {
 	godotenv.Load(".env")
 
 	bot := slacker.NewClient(os.Getenv("SLACK_BOT_TOKEN"), os.Getenv("SLACK_APP_TOKEN"))
-
+	client := witai.NewClient(os.Getenv("WIT_AI_TOKEN"))
+	wolframClient := &wolfram.Client{AppID: os.Getenv("WOLFRAM_APP_ID")}
 	go printCommandEvents(bot.CommandEvents())
+
+	bot.Command("query for bot - <message>", &slacker.CommandDefinition{
+		Description: "send any question to wolfram",
+		Examples:    []string{"who is the president of Nigeria"},
+		Handler: func(botCtx slacker.BotContext, request slacker.Request, response slacker.ResponseWriter) {
+			query := request.Param("message")
+
+			msg, _ := client.Parse(&witai.MessageRequest{
+				Query: query,
+			})
+			data, _ := json.MarshalIndent(msg, "", "    ")
+			rough := string(data[:])
+			value := gjson.Get(rough, "entities.wit$wolfram_search_query:wolfram_search_query.0.value") //acessed our question value and we will be BLE TO SEND IT TO OUR
+			answer := value.String()
+			res, err := wolframClient.GetSpokentAnswerQuery(answer, wolfram.Metric, 1000)
+			if err != nil {
+				fmt.Println("an error occured")
+			}
+			fmt.Println(value)
+			response.Reply(res)
+		},
+	})
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	err := bot.Listen(ctx)
+
+	if err != nil {
+		log.Fatal(err)
+	}
 }
